d351a7a36df1ca4ac7198fad83ce770a80ad5712
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2022-05-27 17:09:52 -0300

    update models

--- pbl ordenamento/gerador-pl/gerador.R
+++ pbl ordenamento/gerador-pl/gerador.R
@@ -46,8 +46,9 @@ vet = function(
   return(vet)
 }
 
+## Area baseada no talhão ----------------------
 
-## Função objetivo -----------------------------------------
+## Função objetivo
 fo = 'max:'
 for(t in cadastro$Talhao){
   for (c in idadeCorte){
@@ -62,7 +63,7 @@ for(t in cadastro$Talhao){
   }
 }
 
-## Restrição de área ----------------------------------------
+## Restrição de área
 resarea = 'S.A.'
 for(t in cadastro$Talhao){
   
@@ -81,8 +82,8 @@ for(t in cadastro$Talhao){
 }
 rm(t, c, areaTemp)
 
-## Restrição produção mínima ---------------------------------
-matrizProducao = function(
+## Restrição produção mínima
+matrizProducaoTalhao = function(
   idadeCorte, 
   producao, 
   cadastro,
@@ -119,7 +120,7 @@ matrizProducao = function(
   return(temp)
 }
 
-matriz = matrizProducao(idadeCorte = idadeCorte, producao = producao, cadastro = cadastro, horizonte = 10)
+matriz = matrizProducaoTalhao(idadeCorte = idadeCorte, producao = producao, cadastro = cadastro, horizonte = 10)
 
 resproducao = 'S.A.'
 for(h in seq(1:horizonte)-1){
@@ -137,11 +138,98 @@ for(h in seq(1:horizonte)-1){
 
 rm(a, h, prodTemp, restemp)
 
-## Corte por talhão -------------------------------
-
 resbin = "BIN"
 for(t in cadastro$Talhao){
   for (c in idadeCorte){
     resbin = paste(resbin, paste(t,'_IC',c,sep=""))
   }
 }
+
+
+## Corte por hectare -------------------
+
+## Função objetivo -----------------------------------------
+fo = 'max:'
+for(t in cadastro$Talhao){
+  for (c in idadeCorte){
+    
+    fo = paste(fo, round(vet(t, c, producao = producao, cadastro = cadastro, 
+                             receita = 70, taxa = 0.10), 2), paste(t,'_IC',c,sep=""), '+')
+    
+  }
+}
+
+## Restrição de area
+resarea = 'S.A.'
+for(t in cadastro$Talhao){
+  
+  areaTemp = paste('AREA_', t, ":", sep="")
+  
+  for (c in idadeCorte){
+    areaTemp = paste(areaTemp, paste(t,'_IC',c,sep=""), '+')
+    
+  }
+  areaTalhao = cadastro %>%
+    filter(Talhao == t) %>%
+    select(Area)
+  
+  resarea = paste(resarea, areaTemp, "=", as.numeric(areaTalhao), ";")
+  #resarea = paste(resarea, areaTemp, "=", 1, ";")
+}
+rm(t, c, areaTemp)
+
+## Restrição de produção
+matrizProducao = function(
+  idadeCorte, 
+  producao, 
+  cadastro,
+  horizonte = 10){
+  
+  temp = data.frame(alternativa = c(), ano = c(), producao = c())
+  for(h in seq(1:horizonte)-1){
+    for(t in cadastro$Talhao){
+      
+      plantio = cadastro %>%
+        filter(Talhao == t) %>%
+        select(anoPlantio) %>% as.numeric()
+      
+      for(c in idadeCorte){
+        
+        if((year(today())-plantio+h)%%c == 0){
+          a = paste(t,'_IC',c,sep="")
+          b = year(today()) + h
+          volTalhao = producao %>%
+            filter(Sitio == as.numeric(cadastro[cadastro$Talhao == t,'Sitio'])) %>%
+            filter(MatGen == as.character(cadastro[cadastro$Talhao == t,'MatGen'])) %>%
+            filter(Idade == c) %>%
+            select(m3ha) %>% as.numeric()
+          areaTalhao = cadastro %>%
+            filter(Talhao == t) %>%
+            select(Area) %>% as.numeric()
+          c =  volTalhao
+          temp = rbind(temp, data.frame(alternativa = a, ano = b, producao = c))
+        }
+        
+      }
+    }
+  }
+  return(temp)
+}
+
+matriz = matrizProducao(idadeCorte = idadeCorte, producao = producao, cadastro = cadastro, horizonte = 10)
+
+resproducao = 'S.A.'
+for(h in seq(1:horizonte)-1){
+  temp = matriz %>%
+    filter(ano == year(today()) + h)
+  restemp = paste('Prod', year(today()) + h, ':', sep="")
+  for(a in temp$alternativa){
+    prodTemp = temp %>%
+      filter(alternativa == a) %>%
+      select(producao) %>% as.numeric()
+    restemp = paste(restemp, prodTemp, a, "+") 
+  }
+  resproducao = paste(resproducao, restemp, ">=", meta, ";")
+}
+
+rm(a, h, prodTemp, restemp)
